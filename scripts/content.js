08528184888a1573c0ea8b2dbf2bd0c2c334ff27
oybek.anapiyaev <oybek.anapiyaev@zozo.com>
2024-10-13 21:15:15 +0900

    add logic to support Japanese&chinese characters by counting char instead of words with whitespace between them

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,28 @@
 const article = document.querySelector("article");
 
-// `document.querySelector` may return null if the selector doesn't match anything.
 if (article) {
   const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+
+  // Regular expression for non-Japanese (counting words)
+  const wordMatchRegExp = /[^\s]+/g;
+  // Regular expression for Japanese (counting characters)
+  const japaneseMatchRegExp = /[\u3040-\u30FF\u4E00-\u9FFF\uFF66-\uFF9F]/g;
+
+  // Match non-Japanese words
   const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
+  // Match Japanese characters
+  const japaneseChars = text.matchAll(japaneseMatchRegExp);
+
+  // Convert both iterators to arrays and get their lengths
   const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 100);
+  const japaneseCharCount = [...japaneseChars].length;
+
+  // Estimate reading time for non-Japanese (100 words per minute) 
+  // and Japanese (400 characters per minute)
+  const readingTime = Math.round(wordCount / 150) + Math.round(japaneseCharCount / 400);
+
+  // Create and style the reading time badge
   const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
   badge.classList.add("color-secondary-text", "type--caption");
   badge.textContent = `⏱️ ${readingTime} min read`;
 
